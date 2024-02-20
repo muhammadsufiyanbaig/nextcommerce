cc13adb28b008b8dc6b38e5f798a65323a8aaee0
muhammadsufiyanbaig <send.sufiyan@gmail.com>
2024-02-20 15:44:00 +0500

    changing

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -25,7 +25,7 @@ const Categorypage = async ({ params }: { params: { category: string } }) => {
         <div className="bg-white py-6 sm:py-8 lg:py-12">
           <div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
           <div className="mb-10 md:mb-16">
-          <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Our {params.category}'s Products</h2>
+          <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Products for {params.category} </h2>
           <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">This is a section of some simple
            filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
         </div>
